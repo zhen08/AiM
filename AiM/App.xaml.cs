9e0f706e08c5e3707ad55a9e6b9d9eea0a8a5f48
zhen <zhen08@gmail.com>
2023-02-05 13:55:16 +0800

    Check in empty new VS project.

---
+++ AiM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AiM;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AiM/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AiM;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
